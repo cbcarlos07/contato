d1ca874709b7d4a2c7dc40db7abe45c260b09a79
master <ham139>
2015-08-06 10:34:58 -0400

    Atualização

---
+++ app/model/Contato.js
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 06/08/2015.
+ */
+Ext.define('ExtMVC.model.Contato',{
+    extend: 'Ext.data.Model',
+    fields: ['id', 'name', 'phone', 'email']
+});
+
---
+++ app/store/Contatos.js
@@ -0,0 +1,31 @@
+
+Ext.define('ExtMVC.store.Contatos', {
+    extend: 'Ext.data.Store',
+    model: 'ExtMVC.model.Contato',
+    autoLoad: true,
+    pageSize: 35,
+    autoLoad: {start: 0, limit: 35},
+
+    proxy: {
+        type: 'ajax',
+        api: {
+            create: 'php/criaContato.php',
+            read: 'php/listaContatos.php',
+            update: 'php/atualizaContato.php',
+            destroy: 'php/deletaContato.php',
+        },
+        reader: {
+            type: 'json',
+            root: 'contatos',
+            successProperty: 'success'
+        },
+        writer: {
+            type: 'json',
+            writeAllFields: true,
+            encode: true,
+            root: 'contatos'
+        }
+    }
+});/**
+ * Created by admin on 06/08/2015.
+ */
